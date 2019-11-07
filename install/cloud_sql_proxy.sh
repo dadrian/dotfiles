aa43f071ce4d02fe0187edf3578ef80ed8a588f6
David Adrian <davidadrian@dadrian-mbp.local>
2019-11-06 23:08:47 -0500

    Remove old intall directory

--- install/cloud_sql_proxy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-mkdir -p $HOME/.bin
-CLOUD_SQL_INSTALL_PATH="$HOME/.bin/cloud_sql_proxy"
-curl -o $CLOUD_SQL_INSTALL_PATH https://dl.google.com/cloudsql/cloud_sql_proxy.darwin.amd64
-chmod a+x $CLOUD_SQL_INSTALL_PATH
-
