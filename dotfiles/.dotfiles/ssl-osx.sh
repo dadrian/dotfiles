3263bca3c5ff236ee48a658e91410dacac60abb8
David Adrian <davidcadrian@gmail.com>
2020-03-30 17:06:42 -0400

    Fix linking OpenSSL on OS X

--- dotfiles/.dotfiles/ssl-osx.sh
+++ dotfiles/.dotfiles/ssl-osx.sh
@@ -1,3 +1,3 @@
 if [ $(uname -s) = "Darwin" ]; then
-    (echo $LIBRARY_PATH | grep '/usr/local/opt/openssl@1.1/lib/') || export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/opt/openssl@1.1/lib/
+    (echo $LIBRARY_PATH | grep '/usr/local/opt/openssl@1.1/lib/' > /dev/null) || export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/opt/openssl@1.1/lib/:/usr/local/lib
 fi
