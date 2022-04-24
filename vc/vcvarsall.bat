0270202d9c85464fd34d0b748f8e4bf14402f7b6
David Adrian <davidcadrian@gmail.com>
2022-04-24 13:06:11 -0600

    Update vcvarsall scripting for VS2022

--- vc/vcvarsall.bat
+++ vc/vcvarsall.bat
@@ -3,5 +3,8 @@
 REM The actual script is likely in: C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build
 
 SET COMMUNITY2019="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build"
+SET COMMUNITY2022="C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build"
 
-IF EXIST %COMMUNITY2019% %COMMUNITY2019%\vcvarsall.bat x64
+SET VCDIR=%COMMUNITY2022%
+
+IF EXIST %VCDIR% %VCDIR%\vcvarsall.bat x64
