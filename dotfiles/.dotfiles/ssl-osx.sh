74708b624c378837ad8cf5e447fccd126bbb35ac
David Adrian <davidcadrian@gmail.com>
2023-02-10 13:10:36 -0700

    Clean up openssl handling on OSX

--- dotfiles/.dotfiles/ssl-osx.sh
+++ dotfiles/.dotfiles/ssl-osx.sh
@@ -1,3 +1,7 @@
-if [ $(uname -s) = "Darwin" ]; then
-    (echo $LIBRARY_PATH | grep '/usr/local/opt/openssl@1.1/lib/' > /dev/null) || export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/opt/openssl@1.1/lib/:/usr/local/lib
-fi
+# Let's leave this disabled for a bit and see if we need it any more.
+#
+# if [ $(uname -s) = "Darwin" ]; then
+#   if [ -d "/usr/local/opt/openssl@1.1/lib" ]; then
+#     (echo $LIBRARY_PATH | grep '/usr/local/opt/openssl@1.1/lib/' > /dev/null) || export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/opt/openssl@1.1/lib/:/usr/local/lib
+#   fi
+# fi
