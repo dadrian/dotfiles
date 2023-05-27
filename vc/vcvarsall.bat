a3ff39c3a8b4298542859e7fd1ba1b86c77419d5
David Adrian <davidcadrian@gmail.com>
2023-05-26 22:26:49 -0600

    Add support for VC Build Tools

--- vc/vcvarsall.bat
+++ vc/vcvarsall.bat
@@ -1,10 +1,10 @@
 @echo off
-
 REM The actual script is likely in: C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build
 
+SET BUILDTOOLS2022="C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build"
 SET COMMUNITY2019="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build"
 SET COMMUNITY2022="C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build"
 
-SET VCDIR=%COMMUNITY2022%
+SET VCDIR=%BUILDTOOLS2022%
 
 IF EXIST %VCDIR% %VCDIR%\vcvarsall.bat x64
