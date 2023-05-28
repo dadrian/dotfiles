d2481611a3faaad5a935ffd6719e3509de84f8a1
David Adrian <davidcadrian@gmail.com>
2023-05-27 18:28:55 -0600

    PowerShell cleanups

--- windows-ps.ps1
+++ windows-ps.ps1
@@ -1,4 +1,6 @@
-Copy-Item .\PowerShell\Profile.ps1 $profile
+$ErrorActionPreference="Stop"
+New-Item -ItemType File -Path $profile.CurrentUserAllHosts -Force
+Copy-Item -Path .\PowerShell\Profile.ps1 -Destination $profile.CurrentUserAllHosts -Force
 ROBOCOPY ${PSScriptRoot}\nvim\nvim $HOME\AppData\Local\nvim /E /XD $HOME\AppData\Local\nvim\plugged
 ROBOCOPY ${PSScriptRoot}\vc $HOME\bin vcvarsall.bat
 ROBOCOPY ${PSScriptRoot}\vscode\user $HOME\scoop\apps\vscode\current\data\user-data\User settings.json
